f794683bb02f7b428f3e3f53f160fcf05c0ecc38
Akira Sosa <akirasosa@gmail.com>
2011-08-20 22:11:28 +0900

    git submodule の untracked content 対策

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 work_dir=$(dirname $0)
-find `pwd` -maxdepth 1 -name '.*' | grep -v ".git$" | grep -v ".gitmodules" | grep -v ".gitignore" | xargs -I{} ln -fs {} $HOME
+find `pwd` -maxdepth 1 -name '.*' | grep -v ".git$" | grep -v ".gitmodules" | xargs -I{} ln -fs {} $HOME
 
