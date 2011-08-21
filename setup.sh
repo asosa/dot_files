20ff43e7777735474316272d23bfe4cf1fe6d9ea
Akira Sosa <akirasosa@gmail.com>
2011-08-21 23:40:01 +0900

    clipboard 共有用スクリプト追加

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-work_dir=$(dirname $0)
-find `pwd` -maxdepth 1 -name '.*' | grep -v ".git$" | grep -v ".gitmodules" | xargs -I{} ln -fs {} $HOME
+cd `dirname $0`
 
+find `pwd` -maxdepth 1 -name '.*' | grep -v ".git$" | grep -v ".gitmodules" | xargs -I{} ln -fs {} $HOME
+find `pwd`/bin -type f | xargs -I{} ln -fs {} /usr/local/bin
