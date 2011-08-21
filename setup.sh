d2848608dd99ceb50fd85cdbc36fdeb09df3a48f
Akira Sosa <akirasosa@gmail.com>
2011-08-21 23:41:41 +0900

    権限

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ cd `dirname $0`
 
 find `pwd` -maxdepth 1 -name '.*' | grep -v ".git$" | grep -v ".gitmodules" | xargs -I{} ln -fs {} $HOME
 find `pwd`/bin -type f | xargs -I{} ln -fs {} /usr/local/bin
+chmod 777 bin/*.sh
