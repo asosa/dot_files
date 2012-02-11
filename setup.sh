8d4ddba4c872898c71d89eb4ed7bda53e1e2ad36
Akira Sosa <akirasosa@gmail.com>
2012-02-12 01:00:43 +0900

    setup.sh を修正

--- setup.sh
+++ setup.sh
@@ -2,4 +2,14 @@
 
 cd `dirname $0`
 
-find `pwd` -maxdepth 1 -name '.*' | grep -v ".git$" | grep -v ".gitmodules" | xargs -I{} ln -fs {} $HOME
+find `pwd` -maxdepth 1 -mindepth 1 -name '.*' | \
+  grep -v ".git$" | \
+  grep -v ".gitmodules" | \
+  xargs -I{} ln -fs {} $HOME
+
+git submodule init && git submodule update
+
+vim +BundleInstall +qall
+
+echo "You may want to ..."
+echo "cp `pwd`/zshenv.example ~/.zshenv && vim ~/.zshenv"
